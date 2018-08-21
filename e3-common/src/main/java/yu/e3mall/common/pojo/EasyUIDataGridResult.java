9e87cf728a5032e721486d0567b57036af01d143
yws <1353228615@qq.com>
2018-08-21 16:44:38 +0800

    tools module

---
+++ e3-common/src/main/java/yu/e3mall/common/pojo/EasyUIDataGridResult.java
@@ -0,0 +1,29 @@
+package yu.e3mall.common.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class EasyUIDataGridResult implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long total;
+	private List<?> rows;
+
+	public Long getTotal() {
+		return total;
+	}
+
+	public void setTotal(Long total) {
+		this.total = total;
+	}
+
+	public List<?> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<?> rows) {
+		this.rows = rows;
+	}
+}
---
+++ e3-common/src/main/java/yu/e3mall/common/pojo/SearchResult.java
@@ -0,0 +1,39 @@
+package yu.e3mall.common.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SearchResult implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long recordCount;
+	private Integer totalPages;
+	private List<SearchItem> itemList;
+
+	public Long getRecordCount() {
+		return recordCount;
+	}
+
+	public void setRecordCount(Long recordCount) {
+		this.recordCount = recordCount;
+	}
+
+	public Integer getTotalPages() {
+		return totalPages;
+	}
+
+	public void setTotalPages(Integer totalPages) {
+		this.totalPages = totalPages;
+	}
+
+	public List<SearchItem> getItemList() {
+		return itemList;
+	}
+
+	public void setItemList(List<SearchItem> itemList) {
+		this.itemList = itemList;
+	}
+
+}
